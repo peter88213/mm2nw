65bc7210a7644e94cd6d3970764a6c8a751dafba
peter88213 <peter.88213@t-online.de>
2023-06-06 19:47:30 +0200

    Prepare v0.1.1
    Strip comments and docstrings on building the script.
    
    based on:
    - PyWriter v12.12.0
    - yw2nwlib v1.0.0
    - mm2yw7lib v0.4.3

--- tools/build_mm2nw.py
+++ tools/build_mm2nw.py
@@ -8,6 +8,8 @@ Published under the MIT License (https://opensource.org/licenses/mit-license.php
 """
 import inliner
 
+VERSION = '0.1.1'
+
 SRC = '../src/'
 BUILD = '../'
 SOURCE_FILE = f'{SRC}mm2nw_.py'
@@ -19,6 +21,10 @@ def main():
     inliner.run(TARGET_FILE, TARGET_FILE, 'yw2nwlib', '../src/')
     inliner.run(TARGET_FILE, TARGET_FILE, 'mm2yw7lib', '../src/')
     inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
+    with open(TARGET_FILE, 'r', encoding='utf-8') as f:
+        script = f.read()
+    with open(TARGET_FILE, 'w', encoding='utf-8', newline='\n') as f:
+        f.write(script.replace('@release', VERSION))
     print('Done.')
 
 
